a5b122242fb923b4d1ca66b81351fd272000184b
João Gabriel Colodetti do Carmo <jgcolodetti13@hotmail.com>
2022-05-23 10:15:53 -0300

    exercicio 3 desafio

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,13 +5,13 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
     let novoArray = []
-    for(let i = array.length-1; i >= 0; i--){
+    for (let i = array.length - 1; i >= 0; i--) {
         novoArray.push(array[i])
     }
     return novoArray
@@ -19,22 +19,34 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+    let x = false
+    while (!x) {
+        x = true
+        for (let i = 1; i < array.length; i += 1) {
+            if (array[i - 1] > array[i]) {
+                x = false
+                let min = array[i - 1]
+                array[i - 1] = array[i]
+                array[i] = min
+            }
+        }
+    }
+    return array
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+
 }
 
 // EXERCÍCIO 07
@@ -44,7 +56,7 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -54,27 +66,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -84,10 +96,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
