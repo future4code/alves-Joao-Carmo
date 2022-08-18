816335d915edbb1ab265d7b0875798c68ddad8c7
João Gabriel Colodetti do Carmo <jgcolodetti13@hotmail.com>
2022-08-18 00:13:22 -0300

    fluxo-de-dados-backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,25 @@
+type obj = {
+    id: string,
+    name: string,
+    price: number
+}
+
+const arrayObj: obj[] = [
+    {
+        id: '1',
+        name: 'prod1',
+        price: 10
+    },
+    {
+        id: '2',
+        name: 'prod2',
+        price: 15
+    },
+    {
+        id: '3',
+        name: 'prod3',
+        price: 20
+    }
+]
+
+export default arrayObj
