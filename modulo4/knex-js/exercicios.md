6f903f3633616045ecf1368241f0806c9e4b64fd
João Gabriel Colodetti do Carmo <jgcolodetti13@hotmail.com>
2022-08-26 22:14:44 -0300

    knex-js

---
+++ modulo4/knex-js/exercicios.md
@@ -0,0 +1,85 @@
+## Exercício 1
+### a)
+O método raw nos permite usar linguagem SQL para fazer querys. A resposta vem em um array e as informações que queremos estão na primeira posição desse array.
+
+### b) 
+    const getActorByName = async (name: string): Promise<any> => {
+    const result = await connection.raw(`
+        SELECT * FROM Actor WHERE name = '${name}'
+    `)
+
+        return result
+    }
+
+### c)
+    const countActorsByGender = async (gender: string): Promise<any> => {
+    const result = await connection.raw(`
+        SELECT COUNT(*) FROM Actor WHERE gender = "${gender}"
+    `)
+
+        return result[0][0].count
+    }
+
+## Exercício 2
+### a)
+    const updateSalary = async (
+    id: string,
+    salary: number,
+    ): Promise<void> => {
+    await connection("Actor")
+        .update({
+        salary: salary,
+        })
+        .where("id", id);
+    };
+
+### b) 
+    const deleteActor = async (
+    id: string
+    ): Promise<void> => {
+    await connection("Actor")
+        .delete()
+        .where("id", id);
+    };
+
+### c)
+    const avgSalary = async (
+    gender: string
+    ): Promise<void> => {
+    await connection("Actor")
+        .avg("salary as average")
+        .where({ gender });
+
+        return result[0].average;
+    };
+
+## Exercício 3
+### a)
+    app.get("/actor/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const actor = await getActorById(id);
+
+        res.status(200).send(actor)
+    } catch (err) {
+        res.status(400).send({
+        message: err.message,
+        });
+    }
+    });
+
+### b)
+    app.get("/actor", async (req, res) => {
+    try {
+        const gender = req.query.gender as string
+        console.log(gender)
+        const count = await countActorsByGender(gender)
+        console.log(count)
+
+        res.status(200).send({quantidade: count})
+    } catch (err: any) {
+        res.status(400).send({
+        message: err.message,
+        });
+    }
+    });
--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -1,53 +1,98 @@
-import express from "express";
+import express, { Express } from "express";
+import knex from "knex";
 import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
 
-const app = express();
+dotenv.config();
 
+export const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+});
+
+const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003")
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost: ${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
 });
-
-import { Request, Response } from "express"
-
-// Esse arquivo seria o index.ts
 
 const getActorById = async (id: string): Promise<any> => {
   const result = await connection.raw(`
     SELECT * FROM Actor WHERE id = '${id}'
   `)
 
-	return result[0][0]
+  return result[0][0]
 }
 
-
-// Assim a chamada funciona fora dos endpoints com .then()/.catch
-getActorById("001")
-	.then(result => {
-		console.log(result)
-	})
-	.catch(err => {
-		console.log(err)
-	});
-
-// Assim a chamada funciona fora dos endpoints com await
-(async () => {
-  console.log(await getActorById("001") )
-})()
-
+const updateSalary = async (
+  id: string,
+  salary: number,
+): Promise<any> => {
+  await connection("Actor")
+    .update({
+      salary: salary,
+    })
+    .where("id", id);
+};
 
 // Ou então podemos chamá-la dentro de um endpoint
-app.get("/users/:id", async (req: Request, res: Response) => {
+app.get("/actor/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const actor = await getActorById(id);
+
+    res.status(200).send(actor)
+  } catch (err: any) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+app.put("/actor/:id", async (req, res) => {
   try {
     const id = req.params.id
-
-    console.log(await getActorById(id))
-
-    res.end()
+    const salary = Number(req.body.salary)
+    await updateSalary(id, salary)
+    res.status(200).send("Salário atualizado com sucessso.")
   } catch (error: any) {
-		console.log(error.message)
+    console.log(error.message)
     res.status(500).send("Unexpected error")
   }
-}) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+})
+
+const countActorsByGender = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+      SELECT COUNT(*) FROM Actor WHERE gender = "${gender}"
+  `)
+  return result[0][0]
+}
+
+app.get("/actor", async (req, res) => {
+  try {
+    const gender = req.query.gender as string
+    console.log(gender)
+    const count = await countActorsByGender(gender)
+    console.log(count)
+
+    res.status(200).send({quantidade: count})
+  } catch (err: any) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
